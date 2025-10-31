fe49bbeb0d3a546864090ca504cc85ff817fca16
rlagudwls5518 <rlagudwls5518@naver.com>
2025-10-31 16:28:49 +0900

    refactor(Calculator) : 예외처리로직 삭제

--- src/main/java/lotto/model/Calculator.java
+++ src/main/java/lotto/model/Calculator.java
@@ -11,7 +11,6 @@ public class Calculator {
     private final Map<ResultRank, Integer> resultCount = new HashMap<>();
 
     public Calculator(List<Lotto> Lottos, List<Integer> winNumbers, int bonusNumber) {
-        validate(bonusNumber);
         this.Lottos = Lottos;
         this.winNumbers = winNumbers;
         this.bonusNumber = bonusNumber;
@@ -42,23 +41,6 @@ public class Calculator {
         if (matchCount == 4) return ResultRank.FOURTH;
         if (matchCount == 3) return ResultRank.FIFTH;
         return null;
-    }
-
-    private void validate(int bonusNumber) {
-        validateBonusNumberRange(bonusNumber);
-        validateBonusNumberDuplication(bonusNumber);
-    }
-
-    private void validateBonusNumberRange(int bonusNumber){
-        if(bonusNumber > 45 || bonusNumber == 0){
-            throw new IllegalArgumentException("[ERROR] 보너스숫자는 1~45까지 입니다.");
-        }
-    }
-
-    private void validateBonusNumberDuplication(int bonusNumber){
-        if(winNumbers.contains(bonusNumber)){
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
-        }
     }
 
 
