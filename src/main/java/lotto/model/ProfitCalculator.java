c3ef982e7ea2b4e66fbc1f888b2d733b1a0c411f
rlagudwls5518 <rlagudwls5518@naver.com>
2025-10-31 13:13:17 +0900

    feat(ProfitCalculator) : 수익률 계산 클래스 구현

---
+++ src/main/java/lotto/model/ProfitCalculator.java
@@ -0,0 +1,26 @@
+package lotto.model;
+
+import java.util.Map;
+
+public class ProfitCalculator {
+    private final int purchaseAmount;
+    private final Map<ResultRank, Integer> resultCount;
+
+    public ProfitCalculator(int purchaseAmount, Map<ResultRank, Integer> resultCount){
+        this.purchaseAmount = purchaseAmount;
+        this.resultCount = resultCount;
+    }
+
+    public double calculateProfitRate() {
+        long totalReward = 0;
+
+        for (ResultRank rank : resultCount.keySet()) {
+            int count = resultCount.get(rank);
+            if (rank == null) continue;
+            totalReward += (long) rank.getReward() * count;
+        }
+
+        return ((double) totalReward / purchaseAmount) * 100;
+    }
+
+}
