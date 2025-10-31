bbc39324c2beb610b7b60a31fc4e24d54315d5f9
rlagudwls5518 <rlagudwls5518@naver.com>
2025-10-31 10:38:01 +0900

    feat(OutputView) : 출력 클래스 구현
    - 입력메시지 출력 구현
    - 랜덤 로또번호 출력 구현
    - 당첨 통계 출력 구현
    - 수익률 출력 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,56 @@
+package lotto.view;
+
+import java.util.List;
+import java.util.Map;
+import lotto.model.Lotto;
+import lotto.model.ResultRank;
+
+public class OutputView {
+
+    public void printPurchaseCount(int count){
+        System.out.println(count+"개를 구매했습니다.");
+    }
+
+    public void printRandomLotto(List<Lotto>numbers){
+        for(Lotto number : numbers){
+            System.out.println(number);
+        }
+
+    }
+    public void printInputPrice(){
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+    public void printInputWinLotto(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+    public void printInputBonusNumber(){
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+    public void printLottoResult(Map<ResultRank, Integer> resultCount){
+        System.out.println("당첨통계");
+        System.out.println("---");
+        ResultRank[] ranks = ResultRank.values();
+        for (int i = 0; i < ranks.length; i++) {
+            ResultRank rank = ranks[i];
+            if (rank == null) continue;
+
+            int count = resultCount.getOrDefault(rank, 0);
+
+            if (rank == ResultRank.SECOND) { // 5개 + 보너스볼 일치만 따로 문구 지정
+                System.out.printf("5개 일치, 보너스 볼 일치 (%s원) - %d개%n",
+                        String.format("%,d", rank.getReward()), count);
+                continue;
+            }
+
+            System.out.printf("%d개 일치 (%s원) - %d개%n",
+                    rank.getMatchCount(),
+                    String.format("%,d", rank.getReward()),
+                    count);
+        }
+    }
+
+    public void printlottoProfitRate(double profitRate){
+        System.out.printf("총 수익률은 %.1f%%입니다.%n", profitRate);
+    }
+}
