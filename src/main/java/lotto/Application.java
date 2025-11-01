226cfc7012c6fd126abb7e363f2bfb7cec58ea93
rlagudwls5518 <rlagudwls5518@naver.com>
2025-11-02 00:03:33 +0900

    refactor : 입출력 메서드 static수정으로 인한 생성자 매개변수 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,16 +2,12 @@ package lotto;
 
 import lotto.controller.LottoController;
 import lotto.service.LottoService;
-import lotto.view.InputView;
-import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
         LottoService lottoService = new LottoService();
 
-        LottoController controller = new LottoController(inputView,outputView, lottoService);
+        LottoController controller = new LottoController(lottoService);
         controller.run();
     }
 }
