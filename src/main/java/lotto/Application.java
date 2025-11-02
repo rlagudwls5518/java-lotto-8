b0ca5ba8e6e239b625db846942bd8dcd21d11e5d
rlagudwls5518 <rlagudwls5518@naver.com>
2025-11-02 16:16:27 +0900

    refactor : 객체 추가로인한 코드 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,15 @@
 package lotto;
 
+import lotto.controller.InputHandler;
 import lotto.controller.LottoController;
 import lotto.service.LottoService;
 
 public class Application {
     public static void main(String[] args) {
         LottoService lottoService = new LottoService();
+        InputHandler inputHandler = new InputHandler();
 
-        LottoController controller = new LottoController(lottoService);
+        LottoController controller = new LottoController(lottoService, inputHandler);
         controller.run();
     }
 }
