50d89423d0a05afee55076d3f556df8ff31cbb49
rlagudwls5518 <rlagudwls5518@naver.com>
2025-11-02 22:33:44 +0900

    refactor(docs) : 수익률 반환하는 메서트 테스트 케이스 추가
    - 총 당첨 금액과 구매 금액을 기준으로 수익률을 정확히 계산하는지 테스트
    - 당첨이 없으면 수익률은 0%를 반환하는지 테스트

---
+++ src/test/java/lotto/ProfitCalculatorTest.java
@@ -0,0 +1,51 @@
+package lotto;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.HashMap;
+import java.util.Map;
+import lotto.model.ProfitCalculator;
+import lotto.model.ResultRank;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ProfitCalculatorTest {
+
+
+    @Test
+    @DisplayName("총 당첨 금액과 구매 금액을 기준으로 수익률을 정확히 계산한다")
+    void shouldCalculateCorrectProfitRate() {
+        // given
+        int purchaseAmount = 8000;
+        Map<ResultRank, Integer> resultCount = new HashMap<>();
+
+        resultCount.put(ResultRank.FIFTH,1);// 5등 1개
+        resultCount.put(null,7); // 꽝 7개
+
+        ProfitCalculator profitCalculator = new ProfitCalculator(purchaseAmount, resultCount);
+
+        // 수익률 = (총 상금 / 구매금액) * 100
+        double expectedRate = 62.5;
+        double actualRate = profitCalculator.calculateProfitRate();
+
+        assertThat(expectedRate).isEqualTo(actualRate);
+    }
+
+    @Test
+    @DisplayName("당첨이 없으면 수익률은 0%이다")
+    void shouldReturnZeroProfitRateWhenNoWinning() {
+        // given
+        int purchaseAmount = 8000;
+        Map<ResultRank, Integer> resultCount = new HashMap<>();
+
+
+        resultCount.put(null,8);
+
+        ProfitCalculator profitCalculator = new ProfitCalculator(purchaseAmount, resultCount);
+
+
+        double actualRate = profitCalculator.calculateProfitRate();
+
+        assertThat(actualRate).isZero();
+    }
+}
