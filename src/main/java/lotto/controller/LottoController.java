8d3832276ae04aaab05d2512df7726f2e1eecb6b
rlagudwls5518 <rlagudwls5518@naver.com>
2025-11-03 15:34:40 +0900

    refactor : 중복 검사로직 제거

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -4,7 +4,6 @@ import java.util.Map;
 import lotto.model.Lotto;
 import lotto.model.LottoGenerator;
 import lotto.model.ResultRank;
-import lotto.model.util.Validator;
 import lotto.service.LottoService;
 import lotto.view.OutputView;
 
@@ -26,7 +25,6 @@ public class LottoController {
 
         Lotto winNumbers = inputHandler.inputWinningNumbersUntilValid();
         int bonusNumber = inputHandler.inputBonusNumber();
-        Validator.validateBonusDuplicateWithWinNumbers(bonusNumber,winNumbers);
 
         Map<ResultRank, Integer> result = lottoService.lottoCalculater(lottoGenerator, winNumbers, bonusNumber);
         double profitRate = lottoService.calculateProfitRate(result, price);
