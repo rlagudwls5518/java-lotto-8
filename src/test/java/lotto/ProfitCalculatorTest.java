0b2f71b5c82538280df55de28002e5bb1b35aeb0
rlagudwls5518 <rlagudwls5518@naver.com>
2025-11-03 15:49:14 +0900

    refactor : 테스트 메서드명, 주석추가

--- src/test/java/lotto/ProfitCalculatorTest.java
+++ src/test/java/lotto/ProfitCalculatorTest.java
@@ -14,38 +14,36 @@ public class ProfitCalculatorTest {
 
     @Test
     @DisplayName("총 당첨 금액과 구매 금액을 기준으로 수익률을 정확히 계산한다")
-    void shouldCalculateCorrectProfitRate() {
+    void CalculateCorrectProfitRate() {
         // given
         int purchaseAmount = 8000;
         Map<ResultRank, Integer> resultCount = new HashMap<>();
 
+        // when
         resultCount.put(ResultRank.FIFTH,1);// 5등 1개
         resultCount.put(null,7); // 꽝 7개
-
         ProfitCalculator profitCalculator = new ProfitCalculator(purchaseAmount, resultCount);
-
         // 수익률 = (총 상금 / 구매금액) * 100
         double expectedRate = 62.5;
         double actualRate = profitCalculator.calculateProfitRate();
 
+        //then
         assertThat(expectedRate).isEqualTo(actualRate);
     }
 
     @Test
     @DisplayName("당첨이 없으면 수익률은 0%이다")
-    void shouldReturnZeroProfitRateWhenNoWinning() {
+    void ReturnZeroProfitRateWhenNoWinning() {
         // given
         int purchaseAmount = 8000;
         Map<ResultRank, Integer> resultCount = new HashMap<>();
-
-
         resultCount.put(null,8);
 
+        // when
         ProfitCalculator profitCalculator = new ProfitCalculator(purchaseAmount, resultCount);
-
-
         double actualRate = profitCalculator.calculateProfitRate();
 
+        // then
         assertThat(actualRate).isZero();
     }
 }
