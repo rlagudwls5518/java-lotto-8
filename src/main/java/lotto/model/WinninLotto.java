e3855a49807515b9ea1b4a20ff58360c1abffd82
rlagudwls5518 <rlagudwls5518@naver.com>
2025-11-03 15:35:38 +0900

    refactor : 당첨번호와 보너스번호 검사 클래스 추가

---
+++ src/main/java/lotto/model/WinninLotto.java
@@ -0,0 +1,27 @@
+package lotto.model;
+
+import lotto.model.util.ExceptionMessage;
+
+public class WinninLotto {
+    private final int bonusNumber;
+    private final Lotto winLottos;
+
+    public WinninLotto(int bonusNumber, Lotto winLottos) {
+        validateBonusDuplicateWithWinNumbers(bonusNumber, winLottos);
+        this.bonusNumber = bonusNumber;
+        this.winLottos = winLottos;
+    }
+
+    private static void validateBonusDuplicateWithWinNumbers(int bonusNumber, Lotto winNumbers){
+        if (winNumbers.getNumbers().contains(bonusNumber)) {
+            throw new IllegalArgumentException(ExceptionMessage.DUPLICATED_BONUS_NUMBER);
+        }
+    }
+
+    public Lotto getWinLottos(){
+        return winLottos;
+    }
+    public int getBonusNumber(){
+        return bonusNumber;
+    }
+}
